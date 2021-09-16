8e3bda9bd85c8b677da0655f7ec04d44be38963f
Cooper Beltrami <45991010+glowbase@users.noreply.github.com>
2021-09-16 11:35:14 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import './App.css';
+import mapboxgl, { MapboxEvent } from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2xvd2Jhc2UiLCJhIjoiY2t0M3JqOHhzMGJsODJvczA0ZXZqOTNoNCJ9.kLEke1rhFGuVe9GJqwFuSw';
 
 export default function App() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const strikeForcesList = ['Lambda', 'Iter', 'Wobbly'];
 
-  const [longitude, setLongitude] = useState('151.209900');
-  const [latitude, setLatitude] = useState('-33.865143');
-  const [zoom, setZoom] = useState(13);
-  const [strikeForces, setStrikeForces] = useState(strikeForcesList);
+  const [longitude, setLongitude] = useState(-70.9);
+  const [latitude, setLatitude] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
-    if (map.current) return; // Init one map
-  
+    if (map.current) return; // Only init one map
+
+    // Setup new map with config
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
+      attributionControl: false,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [longitude, latitude],
-      zoom: zoom
+      zoom: zoom,
     });
+
+    // Add map controls
+    map.current.addControl(new mapboxgl.NavigationControl());
+    map.current.addControl(new mapboxgl.GeolocateControl());
   });
 
   return (
-    <div>
-      <div className="banner">
-      <h1 style={{marginTop: 0}}>Surveillance Map</h1>
+    <div className="d-flex">
+      <div className="map-controls shadow">
 
-        <label>Strike Force</label>
-        <select id="strikeForceDropdown" style={{width: '100%'}} onChange={updateVehicleList}>
-          <option defaultValue hidden value="0">Select...</option>
-          {strikeForces.map(strikeForce => {
-            return <option value={strikeForce}>{strikeForce}</option>
-          })}
-        </select>
       </div>
-
       <div ref={mapContainer} className="map-container"></div>
     </div>
   );
-}
-
-function updateVehicleList(e) {
-  const strikeForce = e.target.value;
-
-  console.log(strikeForce);
 }
